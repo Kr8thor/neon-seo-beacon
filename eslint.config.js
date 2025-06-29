08b12589c3629a4a35bff6e5dedfa258c15163af
Kr8thor <leoc.tfa@gmail.com>
2025-06-30 02:30:14 +0900

    Fix: Replace React ESLint config with proper Nuxt/Vue ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,41 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/no-unused-vars": "off",
-    },
+export default createConfigForNuxt({
+  features: {
+    tooling: true,
+    stylistic: {
+      commaDangle: 'never',
+      braceStyle: '1tbs'
+    }
+  },
+  dirs: {
+    src: [
+      './components',
+      './composables', 
+      './layouts',
+      './middleware',
+      './pages',
+      './plugins',
+      './server',
+      './utils'
+    ]
   }
-);
+}).append(
+  {
+    ignores: [
+      'dist/**',
+      '.nuxt/**',
+      '.output/**',
+      'node_modules/**',
+      '*.d.ts'
+    ]
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-root': 'off'
+    }
+  }
+)
